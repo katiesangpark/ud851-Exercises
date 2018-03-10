1f31746cadc25f7203edf747f8e1e0651b9de03d
DESKTOP-K7OPVS1\katie <katie.sang.park@gmail.com>
2018-03-09 16:52:35 -0800

    COMPLETED T06.02-Make a Preference Fragment

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,21 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by katie on 3/8/2018.
+ */
+
+
+
+// COMPLETED (2) Create a class called SettingsFragment that extends PreferenceFragmentCompat
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // COMPLETED (5) In SettingsFragment's onCreatePreferences method add the preference file using the
+        // addPreferencesFromResource method
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
